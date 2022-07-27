5999897fb195807c8788681b11ddf1f6d95895ef
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-07-27 09:35:25 -0300

    Exercício 5

--- 01 EstruturaSequencial/exe04.lua
+++ 01 EstruturaSequencial/exe04.lua
@@ -1,3 +1,16 @@
 -- Faça um Programa que peça as 4 notas bimestrais e mostre a média
 
+os.execute('clear')
 
+io.write('Informe a 1º nota: ')
+local n1 = io.read('n')
+io.write('Informe a 2º nota: ')
+local n2 = io.read('n')
+io.write('Informe a 3º nota: ')
+local n3 = io.read('n')
+io.write('Informe a 4º nota: ')
+local n4 = io.read('n')
+
+local media = (n1 + n2 + n3 + n4) / 4
+
+print('A média das notas informadas é: ' .. media)
