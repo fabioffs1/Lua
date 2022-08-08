926105d7999c0750c93f2fe7e1a67ae5203a051c
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-08-08 11:01:07 -0300

    Exercício 02

---
+++ 02 EstruturaDeDecisao/exe02.lua
@@ -0,0 +1,15 @@
+--  Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+os.execute('clear')
+io.write('Informe um número: ')
+local num = io.read('n')
+
+if num >= 0 then
+    print('O número informado é positivo.')
+else
+    print('O número informado é negativo.')
+end
+
+
+
+
