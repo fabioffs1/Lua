51fcc061d59808b0674ca35fb3bbf1a881b9e8e5
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-08-09 14:50:01 -0300

    Exercício 04

--- 02 EstruturaDeDecisao/exe04.lua
+++ 02 EstruturaDeDecisao/exe04.lua
@@ -1,9 +1,14 @@
 -- Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
 os.execute('clear')
+io.write('Informe uma letra: ')
+local letra = string.upper(io.read())
 
-
-
+if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U' then
+    print('A letra informada é uma vogal.')
+else
+    print('A letra informada é uma consoante.')
+end
 
 
 
