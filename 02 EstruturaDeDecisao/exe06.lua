7ed5cc37e85a479486d0ca48093caf6bb66b3320
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-08-10 17:30:28 -0300

    Exe 06 incompleto

---
+++ 02 EstruturaDeDecisao/exe06.lua
@@ -0,0 +1,19 @@
+--Faça um Programa que leia três números e mostre o maior deles
+
+os.execute('clear')
+io.write('Informe o primeiro número: ')
+local n1 = io.read('n')
+io.write('Informe o segundo número: ')
+local n2 = io.read('n')
+io.write('Informe o terceiro número: ')
+local n3 = io.read('n')
+local maior = 0
+
+
+if condition then
+    
+end
+
+
+
+
