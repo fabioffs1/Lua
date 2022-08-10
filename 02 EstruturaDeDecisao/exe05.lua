5b03fc4da4a294ba3ef178f7f98c422e1e9b79cb
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-08-10 10:39:29 -0300

    Exercício 05

--- 02 EstruturaDeDecisao/exe05.lua
+++ 02 EstruturaDeDecisao/exe05.lua
@@ -3,12 +3,22 @@ A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 A mensagem "Reprovado", se a média for menor do que sete;
 A mensagem "Aprovado com Distinção", se a média for igual a dez.]]
 
-os.execute
+os.execute('clear')
+io.write('Informe a primeira nota: ')
+local n1 = io.read('n')
+io.write('Informe a segunda nota: ')
+local n2 = io.read('n')
+local media = (n1 + n2) / 2
 
+print('Media = ' .. string.format('%.2f', media))
 
-
-
-
+if media == 10 then
+    print('Aprovado com Distinção')
+elseif media >= 7 then
+    print('Aluno aprovado!')
+else
+    print('Aluno reprovado!')
+end
 
 
 
