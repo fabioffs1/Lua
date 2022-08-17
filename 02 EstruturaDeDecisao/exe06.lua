23f9be6f72c746f49e14bfce52dfcf02b4c851e4
Fabio Fernandes <ffs.fernandes1992@gmail.com>
2022-08-17 09:04:42 -0300

    Exercício 06

--- 02 EstruturaDeDecisao/exe06.lua
+++ 02 EstruturaDeDecisao/exe06.lua
@@ -10,8 +10,12 @@ local n3 = io.read('n')
 local maior = 0
 
 
-if condition then
-    
+if n1 > n2 and n1 > n3 then
+    print('O maior número informado é: ' .. n1)
+elseif n2 > n1 and n2 > n3 then
+    print('O maior número informado é: ' .. n2)
+else
+    print('O maior número informado é: ' .. n3)
 end
 
 
