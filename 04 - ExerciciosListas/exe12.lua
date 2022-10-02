33d49d1996bcc99ba45e24de9c7c22884d99c70d
fabioffs1 <ffs.fernandes1992@gmail.com>
2022-10-02 10:38:54 -0300

    Exercício 12

--- 04 - ExerciciosListas/exe12.lua
+++ 04 - ExerciciosListas/exe12.lua
@@ -1,13 +1,31 @@
---[[ Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13
-anos possuem altura inferior à média de altura desses alunos.]]
+--[[ Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.]]
 
 os.execute('clear')
 
-local alunos
+local idades, alturas = {}, {}
+local soma, media = 0, 0
+local cont = 0
 
+for i=1, 3 do
+    io.write('Informe a idade do '..i..'ª aluno: ')
+    idades[i] = io.read('n')
+end
 
+for i=1, 3 do
+    io.write('Informe a altura do '..i..'ª aluno:')
+    alturas[i] = io.read('n')
+    soma = soma + alturas[i]
+end
 
+media = soma / 3
 
+for i=1, 3 do
+    if idades[i] > 13 and alturas[i] < media then
+        cont = cont + 1
+    end
+end
+
+print('Alunos: '..cont)
 
 
 
